017b393d6ee66175177e6dda54498b276380cdeb
[felix.bloch@myy.haaga-helia.fi] <46814780+FelixBloch@users.noreply.github.com>
2019-03-19 09:27:07 +0100

    Adding the react-router exercise

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+              React Router
+          </header>
+        </div>
+        <div>
+          <Link to=  "/"  >Home</Link>{' '}
+          <Link to=  "/about">About</Link>{' '}
+          <Link to=  "/contact">Contact</Link>{' '}
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" render={() => <h1>Contactaddress</h1>}/>
+            <Route render={() => <h1>Page not found</h1>}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
